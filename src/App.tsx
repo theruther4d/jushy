c60592ff483156d7b56c2a6e66443c56f1a14f0a
Josh Rutherford <joshua.rutherford1@gmail.com>
2021-07-18 12:38:01 -0500

    Add sass, start styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Contributions } from "./Contributions";
 
