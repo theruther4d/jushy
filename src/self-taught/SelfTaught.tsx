63792a8eb2603e951a2f434056fa672b83854d7c
Josh Rutherford <joshua.rutherford1@gmail.com>
2021-07-21 14:56:36 -0500

    Styling, fix overflow issue.

--- src/self-taught/SelfTaught.tsx
+++ src/self-taught/SelfTaught.tsx
@@ -11,35 +11,41 @@ export function SelfTaught() {
           under a year. Working at apple in 2.5 years. Team Lead in 3 years.
         </p>
       </main>
-      <figure>
-        <pre>
-          <code>
-            <span className="var">const</span> radians ={" "}
-            <span className="constant">Math</span>.PI * Math.
-            <span className="property">pow</span>(<span className="var">2</span>
-            );
-          </code>
-        </pre>
-        <span className="error" data-content="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~">
+      <aside>
+        <figure>
           <pre>
             <code>
-              <span className="var">const</span> angle = radianz *{" "}
-              <span className="constant">Math</span>.
+              <span className="var">const</span> radians ={" "}
+              <span className="constant">Math</span>.PI * Math.
               <span className="property">pow</span>(
               <span className="var">2</span>
               );
             </code>
           </pre>
-        </span>
-        <figcaption className="offscreen">
-          Some lines of code with an error.
-        </figcaption>
-        <img
-          src={pondering}
-          width={220}
-          alt="A cartoon of my face, pondering the code with an error."
-        />
-      </figure>
+          <span
+            className="error"
+            data-content="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+          >
+            <pre>
+              <code>
+                <span className="var">const</span> angle = radianz *{" "}
+                <span className="constant">Math</span>.
+                <span className="property">pow</span>(
+                <span className="var">2</span>
+                );
+              </code>
+            </pre>
+          </span>
+          <figcaption className="offscreen">
+            Some lines of code with an error.
+          </figcaption>
+          <img
+            src={pondering}
+            width={220}
+            alt="A cartoon of my face, pondering the code with an error."
+          />
+        </figure>
+      </aside>
     </section>
   );
 }
