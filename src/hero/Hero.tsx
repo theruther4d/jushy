d00582576775c6e5d197a63189924520ea25cd8b
Josh Rutherford <joshua.rutherford1@gmail.com>
2021-07-23 15:52:11 -0500

    More bio, tweak copyright, add gradients to cards.

--- src/hero/Hero.tsx
+++ src/hero/Hero.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./hero.scss";
 import waving from "./waving.png";
 
 export function Hero() {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <section className="hero">
       <header>
@@ -20,11 +23,52 @@ export function Hero() {
       </header>
       <main>
         <p>
-          I’m a curiosity-driven developer with over 8 years of experience. Most
-          of that time has been focused on user interface development and web
-          apps. For the past 4 years I've worked primarily with Typescript and
-          React.
+          I’m a curiosity-driven developer with over 8 years of experience. I've
+          spent most of that time focused on building web apps and UI, for the
+          last 5 years working primarily with React and Typescript.{" "}
+          {showMore ? null : (
+            <button
+              className="link"
+              onClick={function toggle() {
+                setShowMore((showing) => !showing);
+              }}
+            >
+              Read the whole story +
+            </button>
+          )}
         </p>
+        <div
+          className={showMore ? "more-content visible" : "more-content"}
+          aria-hidden={!showMore}
+        >
+          <p>
+            My background is in design, and in 2013{" "}
+            <a
+              href="https://blog.codepen.io/2013/08/23/josh-rutherford-gets-a-job-on-codepen/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              I left my design job in Arkansas for a development gig at a
+              startup in San Francisco
+            </a>{" "}
+            after spending the previous year learning web development in my
+            spare time. I got the itch while working on a portfolio site for
+            myself and haven't been able to stop tinkering since.
+          </p>
+          <p>
+            Currently considering individual contributor roles. Especially
+            interested in areas of nature, biology, physics, art, politics, and
+            literature.{" "}
+            <button
+              className="link"
+              onClick={function toggle() {
+                setShowMore((showing) => !showing);
+              }}
+            >
+              Show less -
+            </button>
+          </p>
+        </div>
       </main>
     </section>
   );
--- src/self-taught/SelfTaught.tsx
+++ src/self-taught/SelfTaught.tsx
@@ -7,35 +7,41 @@ export function SelfTaught() {
       <main>
         <h3>Proudly self-taught</h3>
         <p>
-          From zero to landing my first development job in San Francisco in
-          under a year. Working at apple in 2.5 years. Team Lead in 3 years.
+          From 0 to landing my first development job in San Francisco in under a
+          year. Working at apple in 2.5 years. Team Lead in 3.
+        </p>
+        <p>
+          I'm a self-starter who hates to be bored. I love learning new things
+          and my best days are when I'm busy solving tough problems.
         </p>
       </main>
       <aside>
         <figure>
-          <pre>
-            <code>
-              <span className="var">const</span> radians ={" "}
-              <span className="constant">Math</span>.PI * Math.
-              <span className="property">pow</span>(
-              <span className="var">2</span>
-              );
-            </code>
-          </pre>
-          <span
-            className="error"
-            data-content="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-          >
+          <div aria-hidden>
             <pre>
               <code>
-                <span className="var">const</span> angle = radianz *{" "}
-                <span className="constant">Math</span>.
+                <span className="var">const</span> radians ={" "}
+                <span className="constant">Math</span>.PI * Math.
                 <span className="property">pow</span>(
                 <span className="var">2</span>
                 );
               </code>
             </pre>
-          </span>
+            <span
+              className="error"
+              data-content="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            >
+              <pre>
+                <code>
+                  <span className="var">const</span> angle = radianz *{" "}
+                  <span className="constant">Math</span>.
+                  <span className="property">pow</span>(
+                  <span className="var">2</span>
+                  );
+                </code>
+              </pre>
+            </span>
+          </div>
           <figcaption className="offscreen">
             Some lines of code with an error.
           </figcaption>
