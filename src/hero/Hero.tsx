3886195c59b21ff0a6fe7c8898c2a7bb89f7a344
Josh Rutherford <joshua.rutherford1@gmail.com>
2021-07-24 06:48:07 -0500

    Tidy hero layout.

--- src/hero/Hero.tsx
+++ src/hero/Hero.tsx
@@ -21,55 +21,53 @@ export function Hero() {
           <h2>Software Developer</h2>
         </div>
       </header>
-      <main>
+      <p className="content">
+        I’m a curiosity-driven developer with over 8 years of experience. I've
+        spent most of that time focused on building web apps and UI, for the
+        last 5 years working primarily with React and Typescript.{" "}
+        {showMore ? null : (
+          <button
+            className="link"
+            onClick={function toggle() {
+              setShowMore((showing) => !showing);
+            }}
+          >
+            Read the whole story +
+          </button>
+        )}
+      </p>
+      <div
+        className={showMore ? "more-content visible" : "more-content"}
+        aria-hidden={!showMore}
+      >
         <p>
-          I’m a curiosity-driven developer with over 8 years of experience. I've
-          spent most of that time focused on building web apps and UI, for the
-          last 5 years working primarily with React and Typescript.{" "}
-          {showMore ? null : (
-            <button
-              className="link"
-              onClick={function toggle() {
-                setShowMore((showing) => !showing);
-              }}
-            >
-              Read the whole story +
-            </button>
-          )}
+          My background is in design, and in 2013{" "}
+          <a
+            href="https://blog.codepen.io/2013/08/23/josh-rutherford-gets-a-job-on-codepen/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            I left my design job in Arkansas for a development gig at a startup
+            in San Francisco
+          </a>{" "}
+          after spending the previous year learning web development in my spare
+          time. I got the itch while working on a portfolio site for myself and
+          haven't been able to stop tinkering since.
         </p>
-        <div
-          className={showMore ? "more-content visible" : "more-content"}
-          aria-hidden={!showMore}
-        >
-          <p>
-            My background is in design, and in 2013{" "}
-            <a
-              href="https://blog.codepen.io/2013/08/23/josh-rutherford-gets-a-job-on-codepen/"
-              rel="noreferrer"
-              target="_blank"
-            >
-              I left my design job in Arkansas for a development gig at a
-              startup in San Francisco
-            </a>{" "}
-            after spending the previous year learning web development in my
-            spare time. I got the itch while working on a portfolio site for
-            myself and haven't been able to stop tinkering since.
-          </p>
-          <p>
-            Currently considering individual contributor roles. Especially
-            interested in areas of nature, biology, physics, art, politics, and
-            literature.{" "}
-            <button
-              className="link"
-              onClick={function toggle() {
-                setShowMore((showing) => !showing);
-              }}
-            >
-              Show less -
-            </button>
-          </p>
-        </div>
-      </main>
+        <p>
+          Currently considering individual contributor roles. Especially
+          interested in areas of nature, biology, physics, art, politics, and
+          literature.{" "}
+          <button
+            className="link"
+            onClick={function toggle() {
+              setShowMore((showing) => !showing);
+            }}
+          >
+            Show less -
+          </button>
+        </p>
+      </div>
     </section>
   );
 }
